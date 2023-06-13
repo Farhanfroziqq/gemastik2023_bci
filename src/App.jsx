22aaf83f1244d920390c6e99a081022e12a136d9
Farhanfroziqq <farhanfathurroziq@gmail.com>
2023-06-13 14:09:53 +0700

    menambahkan file component di app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Rooms from "./components/Rooms";
+import Offers from "./components/Offers";
+import Plan from "./components/Plan";
+
 function App() {
-	return <div></div>;
+	return <div>
+		<Navbar/>
+		<Hero/>
+		<Offers/>
+		<Plan/>
+		<Rooms/>
+	</div>;
 }
 export default App;
