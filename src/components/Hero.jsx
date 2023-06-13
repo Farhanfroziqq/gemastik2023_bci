f71bcdf22d1dabc37aa3f00fe5e432e07d97b1c5
Farhanfroziqq <farhanfathurroziq@gmail.com>
2023-06-13 16:16:26 +0700

    menambahkan komponen baru untuk landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className="absolute top-0 w-full h-full flex flex-col justify-center text-white">
             <div className="md:left-[10%] max-w-[1100px] m-auto absoulte p-4">
                 <p>All Inclusive</p>
-                <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>Get Your Body Nutrtion NOW</h1>
+                <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>Get Your Body Nutrition NOW</h1>
                 <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aliquid distinctio reprehenderit dolorum voluptates qui, quisquam consequatur obcaecati veniam nisi, delectus aliquam molestiae magni est voluptatum quae doloremque ab. Iure!</p>
             <button className='bg-white text-black '>Click Here To Start</button>
             </div>
