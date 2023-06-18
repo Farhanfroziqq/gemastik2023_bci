d4313ab34bcfb69e3f6b0fa6dac8b9c7df476d98
fajarshiddiqqq <fajarshiddiqqq@gmail.com>
2023-06-18 20:28:50 +0700

    perbaikan link ke halaman login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import LandingPage from "./Pages/LandingPage";
 import Register from "./Pages/Register";
-import Login from "./Pages/Register";
+import Login from "./Pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function app(){
-	return <div>
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<LandingPage/>}/>
-				<Route path="/Register" element={<Register/>}></Route>
-				<Route path="/Login" element={<Login/>}></Route>
-			</Routes>
-
-		
-		</BrowserRouter>
-	</div>
+function App() {
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<LandingPage />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
-export default app
+export default App;
--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,14 +1,14 @@
-import {BiArrowBack} from 'react-icons/bi'
-import { Link } from 'react-router-dom'
+import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const BackButton = () => {
-  return (
-    <div className='inline-block'>
-      <Link to='/' className='w-fit'>
-      <BiArrowBack classname='top-0 left-0' size={25} />
-      </Link>
-    </div>
-  )
-}
+	return (
+		<div className='inline-block'>
+			<Link to='/' className='w-fit'>
+				<BiArrowBack className='top-0 left-0' size={25} />
+			</Link>
+		</div>
+	);
+};
 
-export default BackButton
+export default BackButton;
