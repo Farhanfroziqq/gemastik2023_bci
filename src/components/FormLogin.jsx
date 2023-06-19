4bb87194d77f9e71571f75d58a808aab59f6a51b
fajarshiddiqqq <fajarshiddiqqq@gmail.com>
2023-06-19 15:06:59 +0700

    menghubungkan sign up dengan api

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -38,32 +38,16 @@ const FormLogin = () => {
 						required
 					/>
 				</div>
-				<div>
-					<label
-						htmlFor='Pass-confirm'
-						className="block font-semibold mb-1 after:content-['*'] after:text-primary2 after:ml-0.5"
-					>
-						Password Confirmation
-					</label>
-					<input
-						type='password'
-						className='w-full px-3 py-2 border shadow rounded block text-small placeholder:text-slate-400 text-primary2 font-semibold focus:outline-none focus:ring-primary1 focus:border-primary2 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-pink-700 peer'
-						id='Pass-confirm'
-						placeholder='Password confirmation'
-						name='Pass-confirm'
-						required
-					/>
-				</div>
 				<button
 					type='submit'
-					className=' px-2 py-2 w-full bg-white text-primary1 rounded-lg hover:bg-slate-400 my-4 button shadow-lg'
+					className=' px-2 py-2 w-full bg-white text-primary1 rounded-lg hover:bg-slate-100 my-4 button shadow-lg'
 				>
 					Sign Up
 				</button>
 			</form>
 
 			<div className='flex justify-center my-4 text-blue-500'>
-				<Link to='/register'>Already Have Acount?</Link>
+				<Link to='/register'>Create new account</Link>
 			</div>
 		</div>
 	);
