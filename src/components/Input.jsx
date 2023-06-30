bd5aefbb68ed8fb6fd1c763e5a8aabc43832e3b1
Farhanfroziqq <farhanfathurroziq@gmail.com>
2023-06-30 20:08:10 +0700

    menambahkan input data contact

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -19,37 +19,37 @@ const Input = () => {
           Fill the account registration form.
         </p>
         <p className=" absolute text-red-500 left-[50%] -translate-x-[50%] top-16 text-sm"></p>
-        <h3 className="px-8 font-bold text-xl mb-2">Personal</h3>
-        <hr class="flex-grow border-gray-300 border-1 mx-8 mb-8" />
-        <form className="px-8">
-          <div className="grid grid-cols-3 grid-rows-2 gap-x-4 gap-y-2">
+        <form className="">
+          <h3 className="px-8 font-bold text-xl mb-2">Personal</h3>
+          <hr class="flex-grow border-gray-300 border-1 mx-8 mb-8" />
+          <div className="grid grid-cols-3 grid-rows-2 gap-x-4 gap-y-2 px-8 mb-8">
             <div className="">
               <label
-                htmlFor="user_called"
+                htmlFor="user_full_name"
                 className="block font-semibold mb-1 after:content-['*'] after:text-red-500  after:ml-0.5">
                 Full name
               </label>
               <input
                 type="text"
                 className="w-full px-3 py-2 border shadow-sm rounded h-12 block text-small text-center placeholder:text-slate-400 text-primary2 font-semibold focus:outline-none focus:ring-primary1 focus:border-primary2 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-pink-700 peer"
-                id="user_called"
+                id="user_full_name"
                 placeholder="your name"
-                name="user_called"
+                name="user_full_name"
                 required
               />
             </div>
             <div className="">
               <label
-                for="user_birth"
+                for="user_birth_date"
                 className="block font-semibold mb-1 after:content-['*'] after:text-red-500 after:ml-0.5">
                 Date Of Birth
               </label>
               <input
                 type="date"
                 className="w-full px-3 py-2 border shadow-sm rounded h-12 block text-small text-center placeholder:text-slate-400 text-primary2 font-semibold focus:outline-none focus:ring-primary1 focus:border-primary2 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-pink-700 peer"
-                id="user_birth"
+                id="user_birth_date"
                 placeholder="0"
-                name="user_birth"
+                name="user_birth_date"
                 required
               />
             </div>
@@ -90,7 +90,7 @@ const Input = () => {
                 className="w-full px-3 py-2 border shadow-sm rounded h-12 block text-small text-center placeholder:text-slate-400 text-primary2 font-semibold focus:outline-none focus:ring-primary1 focus:border-primary2 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-pink-700 peer"
                 id="user_height"
                 placeholder="0"
-                name="user_weight"
+                name="user_height"
                 required
               />
             </div>
@@ -138,10 +138,149 @@ const Input = () => {
               </select>
             </div>
           </div>
+          <h3 className="px-8 font-bold text-xl mb-2">Contact</h3>
+          <hr class="flex-grow border-gray-300 border-1 mx-8 mb-8" />
+          <div className="grid grid-cols-3 grid-rows-2 gap-x-4 gap-y-2 px-8 mb-8">
+            <div className="">
+              <label
+                htmlFor="user_email"
+                className="block font-semibold mb-1 after:content-['*'] after:text-red-500  after:ml-0.5">
+                Email
+              </label>
+              <input
+                type="Email"
+                className="w-full px-3 py-2 border shadow-sm rounded h-12 block text-small text-center placeholder:text-slate-400 text-primary2 font-semibold focus:outline-none focus:ring-primary1 focus:border-primary2 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-pink-700 peer"
+                id="user_email"
+                placeholder="your name"
+                name="user_email"
+                required
+              />
+            </div>
+            <div className="">
+              <label
+                for="user_phone"
+                className="block font-semibold mb-1 after:content-['*'] after:text-red-500 after:ml-0.5">
+                Phone number
+              </label>
+              <input
+                type="tel"
+                className="w-full px-3 py-2 border shadow-sm rounded h-12 block text-small text-center placeholder:text-slate-400 text-primary2 font-semibold focus:outline-none focus:ring-primary1 focus:border-primary2 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-pink-700 peer"
+                id="user_phone"
+                pattern="[0-9]{4}-{0-9}{4}-{0-9}{4}"
+                placeholder="your number"
+                name="user_phone"
+                required
+              />
+            </div>
+            <div className="">
+              <label
+                for="user_province"
+                className="block font-semibold after:content-['*'] after:text-red-500 after:ml-0.5 mb-1">
+                Province
+              </label>
+              <select
+                id="user_province"
+                name="user_province"
+                className={`text-${
+                  selectedGender ? "black" : "slate-500/60"
+                } bg-white border border-gray-300 font-semibold text-small text-center rounded h-12 focus:ring-slate-700 focus:border-primary1 block w-full py-2`}
+                onChange={handleGenderChange}
+                required>
+                <option
+                  selected
+                  disabled
+                  className="hidden text-center"
+                  value="Your Province">
+                  {" "}
+                  Your Province{" "}
+                </option>
+                <option value="Kalimantan Barat">Kalimantan Barat</option>
+                <option value="Daerah Istimewa Yogyakarta">
+                  Daerah Istimewa Yogyakarta
+                </option>
+                <option value="Bali">Bali</option>
+                <option value="Brisbane">Brisbane</option>
+              </select>
+            </div>
+            <div className="">
+              <label
+                for="user_city"
+                className="block font-semibold after:content-['*'] after:text-red-500 after:ml-0.5 mb-1">
+                City
+              </label>
+              <select
+                id="user_city"
+                name="user_city"
+                className={`text-${
+                  selectedGender ? "black" : "slate-500/60"
+                } bg-white border border-gray-300 font-semibold text-small text-center rounded h-12 focus:ring-slate-700 focus:border-primary1 block w-full py-2`}
+                onChange={handleGenderChange}
+                required>
+                <option
+                  selected
+                  disabled
+                  className="hidden text-center"
+                  value="Your City">
+                  {" "}
+                  Your City{" "}
+                </option>
+                <option value="Kalimantan Barat">Kalimantan Barat</option>
+                <option value="Daerah Istimewa Yogyakarta">
+                  Daerah Istimewa Yogyakarta
+                </option>
+                <option value="Bali">Bali</option>
+                <option value="Brisbane">Brisbane</option>
+              </select>
+            </div>
+            <div className="">
+              <label
+                for="user_district"
+                className="block font-semibold after:content-['*'] after:text-red-500 after:ml-0.5 mb-1">
+                District
+              </label>
+              <select
+                id="user_district"
+                name="user_district"
+                className={`text-${
+                  selectedGender ? "black" : "slate-500/60"
+                } bg-white border border-gray-300 font-semibold text-small text-center rounded h-12 focus:ring-slate-700 focus:border-primary1 block w-full py-2`}
+                onChange={handleGenderChange}
+                required>
+                <option
+                  selected
+                  disabled
+                  className="hidden text-center"
+                  value="Your District">
+                  {" "}
+                  Your District{" "}
+                </option>
+                <option value="Kalimantan Barat">Kalimantan Barat</option>
+                <option value="Daerah Istimewa Yogyakarta">
+                  Daerah Istimewa Yogyakarta
+                </option>
+                <option value="Bali">Bali</option>
+                <option value="Brisbane">Brisbane</option>
+              </select>
+            </div>
+            <div className="">
+              <label
+                htmlFor="user_address"
+                className="block font-semibold mb-1 after:content-['*'] after:text-red-500  after:ml-0.5">
+                Address
+              </label>
+              <textarea
+                className="w-full px-3 py-2 border shadow-sm rounded h-12 block text-small text-center placeholder:text-slate-400 text-primary2 font-semibold focus:outline-none focus:ring-primary1 focus:border-primary2 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-pink-700 peer"
+                id="user_address"
+                placeholder="your address"
+                name="user_address"
+                required
+              />
+            </div>
+          </div>
           <Link
             to="/dashboard"
             type="submit"
-            className=" px-2 py-2 w-full bg-white text-center rounded-lg hover:bg-slate-100 my-4 button shadow-sm">
+            className=" px-8 py-2 w-full bg-white text-center rounded-lg hover:bg-slate-100 my-4 button shadow-sm">
             Submit
           </Link>
         </form>
